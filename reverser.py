6d0ee4357419bca6e69bd329c35b9b0f99914758
FL <me>
2024-02-25 17:20:21 +0100

    first commit

---
+++ reverser.py
@@ -0,0 +1,8 @@
+def reverse_sequence(input_str):
+    reversed_str = input_str[::-1]
+    return reversed_str
+
+# Example usage:
+input_sequence = input("Enter a sequence of characters and numbers: ")
+reversed_sequence = reverse_sequence(input_sequence)
+print("Reversed Sequence:", reversed_sequence)
---
+++ unconditional.s
@@ -0,0 +1,14 @@
+global _start
+
+section .text
+_start:
+    mov rbx, 2
+    mov rcx, 5
+    jmp func
+loop:
+    imul rbx, rbx
+    loop loop
+func:
+    mov rax, 60
+    mov rdi, 0
+    syscall
